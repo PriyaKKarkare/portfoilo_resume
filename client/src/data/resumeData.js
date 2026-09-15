1ab5057ee972935186080dccd9c9947394250d58
Priya Kashinath Karkare <priya9930688736@gmail.com>
2026-09-15 15:52:20 +0530

    added blogging platform project.

--- client/src/data/resumeData.js
+++ client/src/data/resumeData.js
@@ -87,6 +87,7 @@ export const PROJECT_LINKS = [
   { name: "RMS Portal", url: "https://test-repository-black-seven.vercel.app/" },
   { name: "Online prescription platform", url: "https://doctor-consultation-system-dw3n.vercel.app/" },
   { name: "Authentication System", url: "https://authenticationsystem-seven.vercel.app/" },
+  { name: "Blogging Platform", url: "https://blogging-platform-eight-eta.vercel.app/" },
   { name: "Jewelry E-commerce", url: "https://jewel-website.vercel.app/" },
   { name: "PhotoStudio website", url: "https://curd-work-gold.vercel.app/" },
   { name: "Food Delivery", url: "https://fooddelivery-k2v0uzdkf-priyas-projects-46ef61df.vercel.app/" },
